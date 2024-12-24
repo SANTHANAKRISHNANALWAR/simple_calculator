bb0379c2c6c29b9eab73b9e0e1bd60c4565ac9f5
SANTHANAKRISHNANALWAR <140005781+SANTHANAKRISHNANALWAR@users.noreply.github.com>
2024-12-24 23:16:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+function calculate(){
+    let num1 = parseFloat(document.getElementById('num1').value);
+    console.log(num1);
+    
+    let num2 = parseFloat(document.getElementById('num2').value);
+    console.log(num2);
+
+    let operand = document.getElementById('arithmetic').value;
+    let result =0;
+    if (operand == 'add') {
+        result = num1 + num2;
+    }
+    else if(operand == 'sub'){
+        result = num1 - num2;
+    }
+    else if(operand == 'mul'){
+        result = num1 * num2;
+    }
+    else if(operand == 'divide'){
+        result = num1 / num2;
+    }
+    else if (operand == 'reminder'){
+        result = num1 % num2;
+    }
+
+    document.getElementById('answer').value=result;
+    // document.write(result);
+    // alert(result);
+    // prompt(result);
+
+}
