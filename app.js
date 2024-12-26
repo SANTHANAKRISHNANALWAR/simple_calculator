21d3d536c37e796cb444c764c5b48e022c82e99f
SANTHANAKRISHNANALWAR <140005781+SANTHANAKRISHNANALWAR@users.noreply.github.com>
2024-12-26 10:25:02 +0530

    Update app.js
    updated version

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
-function calculate(){
+function calculate() {
     let num1 = parseFloat(document.getElementById('num1').value);
     console.log(num1);
-    
+
     let num2 = parseFloat(document.getElementById('num2').value);
     console.log(num2);
 
     let operand = document.getElementById('arithmetic').value;
-    let result =0;
+    let result = 0;
     if (operand == 'add') {
         result = num1 + num2;
     }
-    else if(operand == 'sub'){
+    else if (operand == 'sub') {
         result = num1 - num2;
     }
-    else if(operand == 'mul'){
+    else if (operand == 'mul') {
         result = num1 * num2;
     }
-    else if(operand == 'divide'){
+    else if (operand == 'divide') {
         result = num1 / num2;
     }
-    else if (operand == 'reminder'){
+    else if (operand == 'reminder') {
         result = num1 % num2;
     }
 
-    document.getElementById('answer').value=result;
+    console.log(result)
+
+    // if ((isNaN(result))) {
+    //     document.getElementById('answer').value = "";
+    // }
+
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById('answer').value = "Enter both the numbers";
+    }
+    else {
+        document.getElementById('answer').value = result;
+    }
     // document.write(result);
     // alert(result);
     // prompt(result);
